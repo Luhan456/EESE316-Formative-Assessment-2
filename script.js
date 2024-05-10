61a326cccdf58836b671a73f76e09e274beb2956
Luhan456 <166249230+Luhan456@users.noreply.github.com>
2024-05-10 09:17:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,16 +4,20 @@
 // Delete function for calculator
 function del()
 {
+    // Get the value from the input field with ID 'screen'
     var value = document.getElementById('screen').value;
+    // Remove the last character from the value
     document.getElementById('screen').value = value.substr(0, value.length - 1);
 }
 
 // Calculate function
 function calculate()
 {
+    //Get the expression from the input field named 'text' in the form named 'Calculator'
     var expression = document.Calculator.text.value;
     var result;
     try{
+        // Calculate using eval() function
         result = eval(expression);
         document.Calculator.text.value = result;
     }
@@ -22,4 +26,6 @@ function calculate()
     }
 }
 
+// This is to add an event listener to the element with the id 'calculate'
+// Ahen this element is clicked, the calcualte() function will be called
 document.getElementById('calculate').addEventListener('click', calculate);
